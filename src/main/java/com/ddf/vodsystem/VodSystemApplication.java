34811fd981c386a1f75341c98ac90176e935cb33
ThisBirchWood <dylanseandefaoite@gmail.com>
2025-05-05 18:46:03 +0200

    initial commit

---
+++ src/main/java/com/ddf/vodsystem/VodSystemApplication.java
@@ -0,0 +1,13 @@
+package com.ddf.vodsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodSystemApplication.class, args);
+	}
+
+}
